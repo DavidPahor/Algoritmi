184a73457de844d846d367a500cb4c3ae3249503
David Pahor <dpahor11@gmail.com>
2025-09-29 15:26:58 +0200

    Binarno sortiranje

---
+++ Algoritmi/Binrano_Sortiranje.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int p[20] = {};
+    int k, t, n;
+
+    for (int i = 0; i < 20; i++)
+        p[i] = rand() % 100;
+
+    for (k = 0; k < 20; k++) {
+
+        n = k;
+        t = k;
+
+        for (t; t < 20; t++) {
+
+            if (p[t] < p[n])
+                n = t;
+
+
+            int pomoc;
+            pomoc = p[k];
+            p[k] = p[n];
+            p[n] = pomoc;
+        }
+        cout << p[k] << " ";
+    }
+}
