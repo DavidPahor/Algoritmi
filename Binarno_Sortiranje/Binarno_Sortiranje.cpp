5a8da2e671126116df886b81e80c70de73f0f90b
David Pahor <dpahor11@gmail.com>
2025-10-01 22:11:21 +0200

    Sortiranje

--- Binarno_Sortiranje/Binarno_Sortiranje.cpp
+++ Binarno_Sortiranje/Binarno_Sortiranje.cpp
@@ -1,20 +1,31 @@
-// Binarno_Sortiranje.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <stdio.h>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int p[20] = {};
+    int k, t, n;
+
+    for (int i = 0; i < 20; i++)
+        p[i] = rand() % 100;
+
+    for (k = 0; k < 20; k++) {
+
+        n = k;
+        t = k;
+
+        for (t; t < 20; t++) {
+
+            if (p[t] < p[n])
+                n = t;
+
+
+            int pomoc;
+            pomoc = p[k];
+            p[k] = p[n];
+            p[n] = pomoc;
+        }
+        cout << p[k] << " ";
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
